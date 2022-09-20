669606acace2d74f457e1104802bb3ea7c3c878c
itsparasbisht <iamparasbisht@gmail.com>
2022-09-20 19:57:59 +0530

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import TextField from "@mui/material/TextField";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.homeContainer__sec1}>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div>
+      <div className={styles.homeContainer__sec2}></div>
+    </div>
+  );
 }
