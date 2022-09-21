53a45616a036d09644d7c0decd324f0b2b61267b
itsparasbisht <iamparasbisht@gmail.com>
2022-09-21 20:45:48 +0530

    chat page

--- pages/chatPage.js
+++ pages/chatPage.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "../styles/ChatPage.module.css";
+
+const avatarSrc =
+  "https://avatars.dicebear.com/api/micah/309.84552351888595.svg?background=%23ffffff";
+const username = "kisen67";
 
 function chatPage() {
-  return <div>chatPage</div>;
+  return (
+    <div className={styles.chatContainer}>
+      <div className={styles.chatContainer__chats}>
+        <div className={styles.chatContainer__chat}>
+          <img src={avatarSrc} alt={`Hi, my self ${username}`} />
+          <div>
+            <b>username</b>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+              cum?
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.chatContainer__sendMessage}></div>
+    </div>
+  );
 }
 
 export default chatPage;
