e3d68d9deb278a1d9851306576e65c9037a70626
itsparasbisht <iamparasbisht@gmail.com>
2022-09-21 20:51:20 +0530

    chat styles

--- pages/chatPage.js
+++ pages/chatPage.js
@@ -6,19 +6,24 @@ const avatarSrc =
 const username = "kisen67";
 
 function chatPage() {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
     <div className={styles.chatContainer}>
       <div className={styles.chatContainer__chats}>
-        <div className={styles.chatContainer__chat}>
-          <img src={avatarSrc} alt={`Hi, my self ${username}`} />
-          <div>
-            <b>username</b>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-              cum?
-            </p>
+        {arr.map((item) => (
+          <div key={item} className={styles.chatContainer__chat}>
+            <img src={avatarSrc} alt={`Hi, my self ${username}`} />
+            <div>
+              <b>username</b>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
+                eum excepturi omnis accusantium quaerat similique quisquam sint
+                incidunt veritatis voluptates.
+              </p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
       <div className={styles.chatContainer__sendMessage}></div>
     </div>
