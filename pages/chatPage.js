028b3a8881d4c860c033c570df5f7468f872dbf8
itsparasbisht <iamparasbisht@gmail.com>
2022-09-21 20:23:20 +0530

    handle submit

---
+++ pages/chatPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function chatPage() {
+  return <div>chatPage</div>;
+}
+
+export default chatPage;
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,16 @@ import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
 
 const ariaLabel = { "aria-label": "description" };
 export default function Home() {
   const [avatar, setAvatar] = useState(null);
   const avatarText = Math.random() * 1000;
+
+  const [username, setUsername] = useState("");
+
+  const router = useRouter();
 
   useEffect(() => {
     getAnAvatar();
@@ -28,6 +33,12 @@ export default function Home() {
     }
   }
 
+  const handleSubmit = () => {
+    sessionStorage.setItem("avatar", avatar);
+    sessionStorage.setItem("username", username);
+    router.push("/chatPage");
+  };
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.homeContainer__sec1}>
@@ -35,16 +46,20 @@ export default function Home() {
         <h3>What should we call you?</h3>
         <Input
           placeholder="***"
-          sx={{ fontSize: "1.5em" }}
+          sx={{ fontSize: "1.5em", color: "red" }}
           inputProps={ariaLabel}
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
         />
         <Button
           disableElevation
           variant="contained"
           size="small"
           sx={{ backgroundColor: "black", marginTop: "10px" }}
+          disabled={username.length > 0 ? false : true}
+          onClick={handleSubmit}
         >
-          {"I'm In"}
+          {"Let's Go /"}
         </Button>
       </div>
       <div className={styles.homeContainer__sec2}></div>
